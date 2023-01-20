4edbd8d41f5000b0dff4c1bc64aeb6bb81c17857
JOSE JOVANNEY SILVA LIMA <100790418+Jovanney@users.noreply.github.com>
2023-01-20 09:01:22 -0300

    Criação das rotas HabitToggle e Summary
    A primeira rota marca um hábito como concluído ou não, já a segunda retorna a quantidade de hábitos concluídos ou disponíveis naquele dia.

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -75,8 +75,85 @@ export async function appRoutes(app: FastifyInstance) {
         }
     })
 
+  //completar ou não uma tarefa
+  app.patch('/habits/:id/toggle', async (request)=> {
+
+    const toggleHabitsParams = z.object({
+      id: z.string().uuid()
+    });
+
+    const { id } = toggleHabitsParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    })
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id, 
+        }
+      }
+    })
+
+    if (dayHabit) {
+      //remover a marcação de completo 
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({ //completar op habito nesse dia
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      })
+    }
     
 
 
+
+  })
+
+  app.get('/summary', async ()=> {
+    // [ {date: 20/01, amount: 5, completed: 1}, {date: 18/01, amount: 2, completed: 2}]
+
+    const summary = await prisma.$queryRaw`
+     SELECT 
+      D.id, 
+      D.date,
+      (
+        SELECT  cast(count(*) as float)
+        FROM day_habits DH
+        WHERE DH.day_id = D.id
+      ) as completed,
+      (
+        SELECT cast(count(*) as float)
+        FROM habit_week_days HWD
+        JOIN habits H
+          ON H.id = HWD.habit_id
+        WHERE 
+          HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+          AND H.created_at <= D.date
+      )as amount
+
+     FROM days D
+    `
+    return summary
+  })
 
 }
